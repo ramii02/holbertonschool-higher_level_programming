3632e31fd5af88a199fd14f0758613c6e7001538
ramii02 <louhibirami11@gmail.com>
2021-05-05 11:50:32 +0200

    Write a program that prints all the names defined by the compiled module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-        import sys
-
-            argc = len(sys.argv)
-                        add = 0
-for j in range(1, argc):
-                        add += int(sys.argv[j])
-            print(add)
+    import hidden_4
+    for h in dir(hidden_4):
+        if not h.startswith("__"):
+            print("{}".format(h))
