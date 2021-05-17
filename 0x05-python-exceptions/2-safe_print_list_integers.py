bf16176e75dba67465437b3b267b9cad9bc0dc63
ramii02 <louhibirami11@gmail.com>
2021-05-18 00:04:23 +0200

    python

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    ule = 0
+    count = 0
     for i in range(0, x):
         try:
            print("{:d}".format(my_list[i]), end="")
-           ule = ule + 1
+           count = count + 1
         except TypeError:
            pass
         except ValueError
            pass
      print()
-     return ule
+     return count
 
