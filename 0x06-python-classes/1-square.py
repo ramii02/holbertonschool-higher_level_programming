4a8a6381dca52433b7e357e96299f9d18b2e8c73
ramii02 <louhibirami11@gmail.com>
2021-05-19 00:45:01 +0200

    1-square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+""" The module is used intro to classes """
+
+
 class Square:
+    """ Write a class Square that defines a square """
     def __init__(self, size=0):
         self.__size = size
