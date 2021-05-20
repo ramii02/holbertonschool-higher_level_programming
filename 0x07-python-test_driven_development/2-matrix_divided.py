b75f2503958dcfe60cca5c506a411f432b1a72da
ramii02 <louhibirami11@gmail.com>
2021-05-20 14:06:34 +0200

    return the div of the elementes of matrix

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" return the div of the elementes of matrix """
+
+
+def matrix_divided(matrix, div):
+    """ return the div of the elementes of matrix """
+
+    m = []
+    if len(matrix) == 0:
+        raise TypeError("matrix must be a matrix (list of lists) "
+                        "of integers/floats")
+    for i in matrix:
+        if len(i) == 0:
+            raise TypeError("matrix must be a matrix (list of lists) "
+                            "of integers/floats")
+        else:
+            m.append(len(i))
+        for x in i:
+            if type(x) is not float and type(x) is not int:
+                raise TypeError("matrix must be a matrix (list of lists) "
+                                "of integers/floats")
+    if len(set(m)) != 1:
+        raise TypeError('Each row of the matrix must have the same size')
+    if type(div) is float or type(div) is int:
+        pass
+    else:
+        raise TypeError('div must be a number')
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
+    return [[round(x/div, 2) for x in j] for j in matrix]
