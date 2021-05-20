31540bbc0f88ee232bf18e2ae414a675f1266660
ramii02 <louhibirami11@gmail.com>
2021-05-20 14:49:40 +0200

    i have to display with '#'

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" I have to display with '#' """
+
+
+def print_square(size):
+""" i have to display with '#' """
+
+    if isinstance(size, int):
+        for i in range(size):
+            for j in range(size):
+                print('#', end="")
+            print()
+    if not isinstance(size, int):
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
