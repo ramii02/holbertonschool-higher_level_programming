e29d268e2102b8e44b4d20b07362eee7223c74cf
ramii02 <louhibirami11@gmail.com>
2021-06-03 21:51:26 +0200

    python

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,10 +3,9 @@
 
 
 def number_of_lines(filename=""):
-    """ line of the number """
+    """ the number of lines """
     i = 0
     with open(filename, 'r') as f:
         for star in f:
             i += 1
     return i
-
