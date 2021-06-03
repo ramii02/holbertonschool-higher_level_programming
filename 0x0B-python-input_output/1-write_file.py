e3d66ce5ef21f7f193c6d045d01da758953361ae
ramii02 <louhibirami11@gmail.com>
2021-06-03 21:34:00 +0200

    python

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -10,3 +10,14 @@ def number_of_lines(filename=""):
             n_lines += 1
 
     return n_lines
+#!/usr/bin/python3
+""" Write a function that returns the number of characters written """
+
+
+def number_of_lines(filename=""):
+    """ line of the number """
+    i = 0
+    with open(filename, 'r') as f:
+        for star in f:
+            i += 1
+    return i
