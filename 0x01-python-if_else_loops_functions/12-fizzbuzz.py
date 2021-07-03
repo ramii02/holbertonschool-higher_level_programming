324391eac09bba135351297934d5f5dcf7f1c820
ramii02 <louhibirami11@gmail.com>
2021-07-04 01:27:36 +0200

    alphabet

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-# numbers from 1 to 100 lowercase ASCII alphabet
+# numbers from lowercase ASCII alphabet
 while c in range(ord('a'), ord('z') + 1):
     print("{}".format(chr(c)), end="")
