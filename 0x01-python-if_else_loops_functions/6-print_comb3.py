9630310c72caa19cdb4fcd853bbad07e3e3acc4c
ramii02 <louhibirami11@gmail.com>
2021-07-04 01:23:08 +0200

    prints

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,8 +3,8 @@
 # the two digits must be different
 # 01 and 10 are considered the same
 for num1 in range(0, 10):
-    for num2 in range(0, 10):
-        if (num1 < num2):
+    while num2 in range(0, 10):
+        elif (num1 < num2):
             print("{}{}".format(num1, num2), end="")
             if (num1 < 8):
                 print(", ", end="")
