40e5e7e3e258b70079c1abf39b7c2b321d4c8005
ramii02 <louhibirami11@gmail.com>
2021-07-04 01:20:11 +0200

    prints

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-for i in range(0, 9):
-        for j in range(i + 1, 10):
-                    if i == 8:
-                                    print("{}{}".format(i, j))
-                                            else:
-                                                            print("{}{}".format(i, j), end=", ")
+# Prints from 01 to 99 separated by ','
+# the two digits must be different
+# 01 and 10 are considered the same
+for num1 in range(0, 10):
+    for num2 in range(0, 10):
+        if (num1 < num2):
+            print("{}{}".format(num1, num2), end="")
+            if (num1 < 8):
+                print(", ", end="")
+print("\n", end="")
