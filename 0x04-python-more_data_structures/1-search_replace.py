aa6543c78dbb46e7dd770f3934708aa3b2141d6e
ramii02 <louhibirami11@gmail.com>
2021-07-04 01:13:37 +0200

    py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 """
-defines the number_of_lines function
+Defining write_file Function
 """
 
 
-def number_of_lines(filename=""):
-    """
-    returns the number of lines of a text file
-    """
-    with open(filename, encoding="utf-8") as file:
-        nlines = 0
-        while line in file:
-            nlines += 1
-    return nlines
+def write_file(filename="", text=""):
+    '''
+    write_file Function
+    '''
+    with open(filename, mode="w") as fl:
+        return fl.write(text)
