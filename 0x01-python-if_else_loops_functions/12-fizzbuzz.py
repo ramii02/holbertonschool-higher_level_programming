aa5070801c9d57e6752020403ca2ae8da7798fcd
ramii02 <louhibirami11@gmail.com>
2021-07-04 01:25:02 +0200

    alphabet

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-def fizzbuzz():
-        for i in range(1, 101):
-                    if i % 15 == 0:
-                                    print("FizzBuzz", end=" ")
-                                            elif i % 3 == 0:
-                                                            print("Fizz", end=" ")
-                                                                    elif i % 5 == 0:
-                                                                                    print("Buzz", end=" ")
-                                                                                            else:
-                                                                                                            print(i, end=" ")
+# alphabet
+while c in range(ord('a'), ord('z') + 1):
+    print("{}".format(chr(c)), end="")
