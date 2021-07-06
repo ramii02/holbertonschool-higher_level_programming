3add277743f705afb2d08137a3d06ee26ac22d21
ramii02 <louhibirami11@gmail.com>
2021-07-06 02:15:06 +0200

    py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, ule):
-    a_dictionary.update({key: ule})
-    return a_dictionary
+def update_dictionary(my_dict, key, value):
+    my_dict.update({key: value})
+    return my_dict
