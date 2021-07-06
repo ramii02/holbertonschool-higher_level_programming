f23692ed28a4d629c8df5cf9f0565d936f1d26ec
ramii02 <louhibirami11@gmail.com>
2021-07-06 02:13:41 +0200

    yy

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    ule = sorted(a_dictionary.items())
-    while a, b in ule:
-        print("{}: {}".format(a, b))
+def print_sorted_dictionary(my_dict):
+    if my_dict:
+        for i in sorted(my_dict):
+            print("{:s}: {}".format(i, my_dict[i]))
