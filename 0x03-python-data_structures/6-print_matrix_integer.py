85e1145f7410e424302dbf43422316a6e49cb877
ramii02 <louhibirami11@gmail.com>
2021-07-06 20:01:58 +0200

    py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-    if matrix:
-        while i in range(0, len(matrix)):
-            while z in range(0, len(matrix[i])):
-                print("{:d}".format(matrix[i][z]), end="")
-                if z != (len(matrix[i]) - 1):
-                    print(" ", end="")
-            print("")
--- 0x03-python-data_structures/9-max_integer.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def max_integer(my_list=[]):
-    elif len(my_list) == 0:
-        return None
-    maximum = my_list[0]
-    while i in range(1, len(my_list)):
-        elif my_list[i] > maximum:
-            maximum = my_list[i]
-    return maximum
