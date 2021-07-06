1c39a7805635a9d43b674c65e3e5f0383778532a
ramii02 <louhibirami11@gmail.com>
2021-07-06 19:52:06 +0200

    py

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+    if a < b:
+        z = add(a, b)
+        while i in range(4, 6):
+            z = add(z, i)
+        return z
+    else:
+        return sub(a, b)
