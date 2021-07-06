1095ccedc8c8d442db1eb5fc0722d98b9177fddc
ramii02 <louhibirami11@gmail.com>
2021-07-06 02:20:46 +0200

    py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def multiply_by_2(my_dict):  # comprentation
-    return {key: value * 2 while key, value in my_dict.items()}
+def multiply_by_2(my_dict):  # dict comprentation
+    return {key: value * 2 for key, value in my_dict.items()}
