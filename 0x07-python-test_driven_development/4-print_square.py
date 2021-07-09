2923017ab68038effa103810189760e2bda41118
ramii02 <louhibirami11@gmail.com>
2021-07-09 22:54:16 +0200

    py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-""" I have to display with '#' """
+"""print_square module"""
 
 
 def print_square(size):
-""" i have to display with '#' """
+    """function that prints square of size size with '#' """
 
-    if isinstance(size, int):
-        for i in range(size):
-            for j in range(size):
-                print('#', end="")
-            print()
-    if not isinstance(size, int):
-        raise TypeError('size must be an integer')
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
     if size < 0:
-        raise ValueError('size must be >= 0')
+        raise TypeError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print('#', end='')
+        print()
