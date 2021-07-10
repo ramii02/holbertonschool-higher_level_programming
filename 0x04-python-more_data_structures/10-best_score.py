b125b17e4c7377d4cfc90ff3c6cb699642f2b3eb
ramii02 <louhibirami11@gmail.com>
2021-07-10 15:47:41 +0200

    py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if a_dictionary == {} or a_dictionary is None:
         return None
     counter = list(a_dictionary.keys())[0]
-    while key in a_dictionary.keys():
+    for key in a_dictionary.keys():
         if a_dictionary[key] > a_dictionary.get(counter):
             counter = key
     return counter
