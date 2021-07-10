8a941259243918603d7a3aaf802746d0ebef1b1a
ramii02 <louhibirami11@gmail.com>
2021-07-10 17:10:33 +0200

    py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-""" square class """
+"""square class"""
 
 
-class Square():
-    """ initialize square with size """
+class Square:
+    """initialize square with size"""
+
     def __init__(self, size=0):
-        """ initialize square with variables"""
-        if type(size) != int:
+        """initialize square with variables"""
+        if type(size) is int:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+        else:
             raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
 
-        def area(self):
-            """find the area of ​​the square"""
-        return self.__size ** 2
+    def area(self):
+        """to find area of square"""
+        return self.__size * self.__size
