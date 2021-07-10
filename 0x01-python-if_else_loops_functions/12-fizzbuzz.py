b317b046ec978d2f8c714be887d12c372b5efe14
ramii02 <louhibirami11@gmail.com>
2021-07-10 15:03:06 +0200

    py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
-# lowercase ASCII alphabet
-for c in range(ord('a'), ord('z') + 1):
-    print("{}".format(chr(c)), end="")
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0:
+            print("Fizz", end="")
+        if i % 5 == 0:
+            print("Buzz", end="")
+        if i % 3 != 0 and i % 5 != 0:
+            print("{:d}".format(i), end="")
+        print(" ", end="")
