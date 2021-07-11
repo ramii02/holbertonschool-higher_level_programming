b72be286a9188d2e2e548893d61b27a209aca7be
ramii02 <louhibirami11@gmail.com>
2021-07-11 20:20:16 +0200

    py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python3
-""" I have to display My name is <first name> <last name> """
+'''class say_my_name'''
 
 
 def say_my_name(first_name, last_name=""):
-    """ I have to display My name is <first name> <last name> """
-
-    fn = 0
-    ln = 0
-    if type(first_name) is str:
-        fn = 1
-    elif not isinstance(first_name, str):
+    '''function that prints My name is <first_name> <last_name>'''
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    if type(last_name) is str:
-        ln = 1
-    elif not isinstance(last_name, str):
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-
-    if fn == 1 and ln == 0:
-        print("My name is {} ".format(first_name))
-    else:
-        print("My name is {} {}".format(first_name, last_name))
+    print("My name is {:s} {:s}".format(first_name, last_name)
