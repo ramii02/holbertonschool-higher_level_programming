8d484cc360b6785511cd12fcc1f12833bbf2ef7a
ramii02 <louhibirami11@gmail.com>
2021-07-14 12:28:39 +0200

    py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-"Function to print squares"
+'''class print_square'''
 
 
 def print_square(size):
-    """Arguments
-    size: size of the int
-    """
+    ''' Print a square with size '''
     if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if size < 0 and type(size) is float:
-        raise TypeError("size must be an integer")
-    for a in range(size):
-        print("#" * size)
+    for i in range(size):
+        print('#' * size)
