6669d7c2739b536323e5fa4cb3dec7f962efe9ac
ramii02 <louhibirami11@gmail.com>
2021-07-14 02:36:29 +0200

    py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,8 +4,8 @@
 
 def number_of_lines(filename=""):
     '''return number of lines of a text'''
-    ule = 0
+    numlines = 0
     with open(filename, 'r', encoding='utf-8') as f:
         for line in f:
-            ule = ule + 1
-    return ule
+            numlines = numlines + 1
+    return numlines
