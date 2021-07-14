5447c8f179d1a23136113f350a841daed1dbba65
ramii02 <louhibirami11@gmail.com>
2021-07-14 02:41:59 +0200

    py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-""" Write a file """
+'''append to a file module'''
 
 
 def append_write(filename="", text=""):
-    """ Write using append """
-    chars_a = 0
-    with open(filename, mode='a', encoding='utf-8') as a_file:
-        chars_a = a_file.write(text)
-        return chars_a
+    '''appends text to end of file'''
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.write(text)
