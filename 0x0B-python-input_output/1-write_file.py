c169355ecdad8e01ce45f4279d2a091c55367d70
ramii02 <louhibirami11@gmail.com>
2021-07-17 13:11:15 +0200

    py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-'''Number of lines module'''
-
-
 def number_of_lines(filename=""):
-    '''return number of lines of a text'''
-    a = 0
-    with open(filename, 'r', encoding='utf-8') as f:
-        while line in f:
-            a = a + 1
-    return a
+    with open(filename, mode='r', encoding='utf-8') as texter:
+        text = texter.read()
+        cont = text.count("\n")
+        return cont
