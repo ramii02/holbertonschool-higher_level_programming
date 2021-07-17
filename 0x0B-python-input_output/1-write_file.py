9572492b08cbd4171579c8c02512a505dc68fcc6
ramii02 <louhibirami11@gmail.com>
2021-07-17 15:27:44 +0200

    py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-def number_of_lines(filename=""):
-    with open(filename, mode='r', encoding='utf-8') as texter:
-        text = texter.read()
-        cont = text.count("\n")
-        return cont
+""" writes string to a text file """
+
+
+def write_file(filename="", text=""):
+    """ write to file """
+    with open(filename, mode='w', encoding='utf-8') as f:
+        a = f.write(text)
+    return a
