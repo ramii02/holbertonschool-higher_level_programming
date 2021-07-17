33d10dc4abc1e9ea19e71076c1e8f25688e4926e
ramii02 <louhibirami11@gmail.com>
2021-07-17 15:30:31 +0200

    pu

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-'''append to a file module'''
+""" returns an object repped by JSON """
 
 
-def append_write(filename="", text=""):
-    '''appends text to end of file'''
-    with open(filename, 'a', encoding='utf-8') as f:
-        return f.write(text)
+import json
+
+
+def from_json_string(my_str):
+    """ returns object """
+    return json.loads(my_str)
