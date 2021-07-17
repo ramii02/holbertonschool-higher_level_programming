8f7c01f1c672408ac165e9c39320a39fa13f1053
ramii02 <louhibirami11@gmail.com>
2021-07-17 12:48:01 +0200

    py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])
+"""Module to find the max integer in a list
 """
-import unittest
-max_integer = __import__('6-max_integer').max_integer
 
-class TestMaxInteger(unittest.TestCase):
-    def valid_tests(self):
-        # positive testing
-        self.assertEqual(max_integer([0, 25, 55, 98]), 98)
-        self.assertEqual(max_integer([1, 2, 98, 3]), 98)
 
-    def negatives(self):
-    # negative testing
-        self.assertEqual(max_integer([-98, -55, -20, -1]), -1)
-        self.assertEqual(max_integer([-1, -2, -98, 3]), 3)
-
-    def strings(self):
-    # string testing
-        self.assertEqual(max_integer("hello"), 'o')
+def max_integer(list=[]):
+    """Function to find and return the max integer in a list of integers
+        If the list is empty, the function returns None
+    """
+    if len(list) == 0:
+        return None
+    result = list[0]
+    i = 1
+    while i < len(list):
+        if list[i] > result:
+            result = list[i]
+        i += 1
+    return result
