81519397e40c494fcf99b2f22e8b7fa851188ab5
ramii02 <louhibirami11@gmail.com>
2021-07-17 16:34:49 +0200

    py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" writes object to text file """
+""" Task 5 """
+import json
 
 
 def save_to_json_file(my_obj, filename):
-    """ save to json """
-    if type(filename) != str:
-        raise TypeError("filename must be a string")
-    import json
-    with open(filename, "w", encoding="utf-8") as f:
+    """
+    function that writes an Object to a text file, using a JSON representation
+    """
+    with open(filename, 'w') as f:
         json.dump(my_obj, f)
