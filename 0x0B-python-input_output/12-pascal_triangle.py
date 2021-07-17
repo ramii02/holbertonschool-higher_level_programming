25defa2aad4130f59b6ff26782adda36c181876a
ramii02 <louhibirami11@gmail.com>
2021-07-17 15:40:27 +0200

    py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''pascal triangle module'''
+
+
+def pascal_triangle(n):
+    '''return a list of integers of pascal triangle '''
+    newlist = []
+    for i in range(n):
+        newlist.append([])
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                newlist[i].append(1)
+            else:
+                newlist[i].append(newlist[i - 1][j - 1] + newlist[i - 1][j])
+    return newlist
