5cc096be4798eb4bf28b70a1e6f7d579fcfe007a
ramii02 <louhibirami11@gmail.com>
2021-07-17 20:04:14 +0200

    py

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,27 +3,27 @@
 /**
  *is_palindrome - function in C that checks if a list is a palindrome.
  *
- *@mu: double pointer to mu
+ *@head: double pointer to head
  *Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
 
-int is_palindrome(listint_t **mu)
+int is_palindrome(listint_t **head)
 {
-	listint_t *ule = *mu;
+	listint_t *aux = *head;
 	int arr[6000];
 	int i = 0, j = 0;
 
-	if (mu == NULL)
+	if (head == NULL)
 		return (0);
-	if (!*mu || (*mu)->next == NULL)
+	if (!*head || (*head)->next == NULL)
 		return (1);
-	for (ule)
+	while (aux)
 	{
-		arr[i] = ule->n;
-		ule = ule->next;
+		arr[i] = aux->n;
+		aux = aux->next;
 		i++;
 	}
-	for (i > j)
+	while (i > j)
 	{
 		if (arr[j] != arr[i - 1])
 			return (0);
